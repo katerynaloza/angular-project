c60c044b375d321612c03fbcd66266305d857db2
Kateryna Loza <katerynaloza2@gmail.com>
2025-09-03 17:29:35 +0000

    додавання залежностей, реалізація маршрутизації та створення початкових компонентів і сервісів

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { ArticleDetailComponent } from './pages/article-detail/article-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'article/:id', component: ArticleDetailComponent },
+    { path: '**', redirectTo: '' },
+];
